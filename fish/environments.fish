67368545a030602d3f4800e5ea03a4beb4d2e3d7
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-04-15 02:27:50 +0530

    Add goroot

--- fish/environments.fish
+++ fish/environments.fish
@@ -9,7 +9,10 @@ set -x NVM_NODEJS_ORG_MIRROR http://nodejs.org/dist
 # Deft notes
 set -x NOTES_DIR /media/lepisma/Dropbox/Journal/
 
-# Python environments
+# Go root
+set -x GOROOT ~/tools/go
+
+# Path
 set PATH ~/tools/anaconda/bin $PATH
 set PATH ~/tools/go/bin $PATH
 
