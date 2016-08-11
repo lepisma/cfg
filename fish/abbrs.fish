4978e0026a8deda5f2500c0106944ffdbc9f3054
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-08-11 23:43:32 +0530

    Stuff

--- fish/abbrs.fish
+++ fish/abbrs.fish
@@ -6,3 +6,9 @@ abbr -a ac "sudo apt-cache"
 
 # Youtube-dl
 abbr -a yt "youtube-dl --external-downloader=aria2c"
+
+# Open things
+abbr -a x "xdg-open"
+
+# Android keyboard init
+abbr -a ak "adb forward tcp:7777 tcp:7777; xdg-open 'http://localhost:7777'"
--- fish/environments.fish
+++ fish/environments.fish
@@ -4,7 +4,7 @@
 set -x BEETSDIR /media/lepisma/Data/Music/
 
 # Deft notes
-set -x NOTES_DIR /media/lepisma/Dropbox/Journal/
+set -x NOTES_DIR /media/lepisma/Data/Dropbox/Journal/
 
 # Go things
 set -x GOROOT ~/tools/go
@@ -12,6 +12,10 @@ set -x GOPATH ~/.go
 
 # Racer
 set -x RUST_SRC_PATH ~/tools/rustc-1.10.0/src
+
+# Personal
+set -x EMAIL abhinav.tushar.vs@gmail.com
+set -x NAME Abhinav Tushar
 
 # Torch
 bass source ~/tools/torch/install/bin/torch-activate
@@ -22,5 +26,11 @@ set PATH ~/tools/go/bin $PATH
 set PATH /usr/local/cuda-7.5/bin $PATH
 set PATH ~/.cargo/bin $PATH
 set PATH ~/tools/processing $PATH
+set PATH ~/.cabal/bin $PATH
+set PATH /opt/cabal/1.22/bin $PATH
+set PATH /opt/ghc/7.10.3/bin $PATH
+set PATH ~/tools/nim-0.14.2/bin $PATH
+set PATH ~/.nimble/bin $PATH
 
+# CUDA
 set LD_LIBRARY_PATH /usr/local/cuda-7.5/lib64: $LD_LIBRARY_PATH
