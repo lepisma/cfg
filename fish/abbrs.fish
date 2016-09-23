b03460f5e43714e00bae0e90a83dafab6425a92e
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-09-22 23:33:13 -0400

    tar abbreviation

--- fish/abbrs.fish
+++ fish/abbrs.fish
@@ -10,6 +10,9 @@ abbr -a yt "youtube-dl --external-downloader=aria2c"
 # Open things
 abbr -a x "xdg-open"
 
+# Holy tar
+abbr -a t "tar xvf"
+
 # Android keyboard init
 abbr -a ak "adb forward tcp:7777 tcp:7777; xdg-open 'http://localhost:7777'"
 
