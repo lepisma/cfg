91ea36a54c0ce139bb2247a72743d25e03dc9fac
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-12-10 17:25:38 -0500

    Add nim bin path

--- fish/.config/fish/environments.fish
+++ fish/.config/fish/environments.fish
@@ -15,6 +15,7 @@ set -x NAME Abhinav Tushar
 
 set PATH ~/bin $PATH
 set PATH ~/tools/anaconda/bin $PATH
+set PATH ~/tools/Nim/bin $PATH
 set PATH ~/.nimble/bin $PATH
 set PATH /usr/local/cuda-8.0/bin $PATH
 
