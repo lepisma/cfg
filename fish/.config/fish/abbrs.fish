e1ec4261d3059ac37d13602fcc0019b39946857b
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-12-11 00:54:02 -0500

    - old gpg alias + vpn alias

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -17,5 +17,5 @@ abbr -a t "tar xvf"
 # Android keyboard init
 abbr -a ak "adb forward tcp:7777 tcp:7777; xdg-open 'http://localhost:7777'"
 
-# Set gpg2 as gpg (14.04)
-alias gpg gpg2
+# VPN
+abbr -a v "sudo openconnect --juniper sslvpn.umass.edu"
