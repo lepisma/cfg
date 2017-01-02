0a6b2ebae700ab682fa81d3e6b82b147408c57a1
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-01-02 01:13:43 -0500

    Huion config commands

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -76,3 +76,15 @@ function do
     return 1
   end
 end
+
+# Tablet default keyset
+function huion-set
+  xsetwacom --set "HUION PenTablet Pad pad" Button 1 "key +ctrl +z -z -ctrl"
+  xsetwacom --set "HUION PenTablet Pad pad" Button 2 "key e"
+  xsetwacom --set "HUION PenTablet Pad pad" Button 3 "key b"
+  xsetwacom --set "HUION PenTablet Pad pad" Button 8 "key +"
+  xsetwacom --set "HUION PenTablet Pad pad" Button 9 "key -"
+  xsetwacom --set "HUION PenTablet Pad pad" Button 10 "key ]"
+  xsetwacom --set "HUION PenTablet Pad pad" Button 11 "key ["
+  xsetwacom --set "HUION PenTablet Pad pad" Button 12 "key p"
+end
