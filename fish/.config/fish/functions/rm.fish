39b984b0f339fc290f8c0070f364398a81006586
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-05-12 01:54:04 -0400

    Borg backup

--- fish/.config/fish/functions/rm.fish
+++ fish/.config/fish/functions/rm.fish
@@ -1,8 +1,7 @@
 function rm -d "Avoid frustration"
   command rm $argv
   if test $status -ne 0
-    echo 'force? [yn] '
-    read -l -n1 ans
+    read -l -n1 -p "set_color -o red; echo -n 'force? [yn] '; set_color normal" ans
     string match -qi y -- $ans; or return 1
   end
   command rm -rf $argv
