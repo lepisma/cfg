73f040cee3c6bd41cabeb7b6e5535c3fc942de2b
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-05-29 05:27:30 -0400

    Pruning

--- kde/konsole/lepisma.profile
+++ kde/konsole/lepisma.profile
@@ -1,6 +1,6 @@
 [Appearance]
-ColorScheme=WhiteOnBlack
-Font=Source Code Pro,9,-1,0,50,0,0,0,0,0
+ColorScheme=Breeze
+Font=Source Code Pro,10,-1,0,50,0,0,0,0,0
 
 [Cursor Options]
 CursorShape=1
@@ -8,8 +8,10 @@ CursorShape=1
 [General]
 Command=/usr/bin/fish
 Directory=/home/lepisma/Desktop
+LocalTabTitleFormat=konsole
 Name=lepisma
 Parent=FALLBACK/
+RemoteTabTitleFormat=konsole
 
 [Interaction Options]
 AutoCopySelectedText=true
--- packages/setup.sh
+++ packages/setup.sh
@@ -24,19 +24,3 @@ pip install -r ./pip
 echo -e "\n>> Installing node packages\n"
 yarn global add $(grep -vE "^\s*#" ./node  | tr "\n" " ")
 echo -e "\n"
-
-echo -e "\n>> Installing cargo packages\n"
-cargo install $(grep -vE "^\s*#" ./cargo  | tr "\n" " ")
-echo -e "\n"
-
-echo -e "\n>> Installing nimble packages\n"
-nimble install $(grep -vE "^\s*#" ./nimble  | tr "\n" " ")
-echo -e "\n"
-
-echo -e "\n>> Installing cabal packages\n"
-cabal install $(grep -vE "^\s*#" ./cabal  | tr "\n" " ")
-echo -e "\n"
-
-echo -e "\n>> Installing roswell packages\n"
-ros install $(grep -vE "^\s*#" ./roswell  | tr "\n" " ")
-echo -e "\n"
