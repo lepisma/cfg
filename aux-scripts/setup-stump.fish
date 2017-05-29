23d6ddcc385b1d31e244da2b93d299a1775f11cf
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-05-29 06:02:48 -0400

    stump contrib module

--- aux-scripts/setup-stump.fish
+++ aux-scripts/setup-stump.fish
@@ -2,7 +2,6 @@
 
 ros build ~/bin/stumpwm.ros
 install -m 0755 ~/bin/stumpwm /usr/local/bin
-
 echo "export KDEWM=/usr/local/bin/stumpwm" > ~/.config/plasma-workspace/env/set_window_manager.sh
-
 rm -f ~/bin/stumpwm
+git clone https://github.com/stumpwm/stumpwm-contrib ~/stumpwm/contrib
