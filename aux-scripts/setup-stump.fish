0407eedfe8385ad8a632a4fa397dd4b09c8e54dd
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-05-31 07:15:14 -0400

    Add stumpwm local modules

--- aux-scripts/setup-stump.fish
+++ aux-scripts/setup-stump.fish
@@ -5,3 +5,5 @@ install -m 0755 ~/bin/stumpwm /usr/local/bin
 echo "export KDEWM=/usr/local/bin/stumpwm" > ~/.config/plasma-workspace/env/set_window_manager.sh
 rm -f ~/bin/stumpwm
 git clone https://github.com/stumpwm/stumpwm-contrib ~/stumpwm/contrib
+mkdir ~/stumpwm/local
+git clone git@github.com/lepisma/swm-calibre ~/stumpwm/local/
