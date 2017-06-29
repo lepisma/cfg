7a8ee9b11ef49e3f08477f4968d0b8588b234a2d
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-06-29 01:16:13 -0400

    Fix indent

--- aux-scripts/setup-4000-scroll.fish
+++ aux-scripts/setup-4000-scroll.fish
@@ -2,8 +2,8 @@
 
 # Setup scroll for microsoft 4000 keyboard
 echo -e "evdev:input:b0003v045Ep00DB*
-KEYBOARD_KEY_c022d=pageup
-KEYBOARD_KEY_c022e=pagedown" | sudo tee /etc/udev/hwdb.d/70-keyboard-local.hwdb > /dev/null
+  KEYBOARD_KEY_c022d=pageup
+  KEYBOARD_KEY_c022e=pagedown" | sudo tee /etc/udev/hwdb.d/70-keyboard-local.hwdb > /dev/null
 
 sudo udevadm hwdb --update
 sudo udevadm control --reload
