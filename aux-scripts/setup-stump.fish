8ae9c1ac84f1d4e7f7c758c4e9252a23bfffae99
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-07-31 00:18:50 +0530

    Fix path for stumpwm.ros

--- aux-scripts/setup-stump.fish
+++ aux-scripts/setup-stump.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/env fish
 
-ros build ~/bin/stumpwm.ros
-install -m 0755 ~/bin/stumpwm /usr/local/bin
+ros build ./stumpwm.ros
+install -m 0755 ./stumpwm /usr/local/bin
 echo "export KDEWM=/usr/local/bin/stumpwm" > ~/.config/plasma-workspace/env/set_window_manager.sh
-rm -f ~/bin/stumpwm
+rm -f ./stumpwm
 git clone https://github.com/stumpwm/stumpwm-contrib ~/stumpwm/contrib
 mkdir ~/stumpwm/local
 git clone git@github.com/lepisma/swm-calibre ~/stumpwm/local/
