e51c99afcfb75b7bf0de8aed3b506ccd0d3ef544
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-07 22:55:45 +0530

    Simple webserver abbr

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -18,6 +18,8 @@ abbr -a v "sudo openconnect --juniper sslvpn.umass.edu"
 
 abbr -a d "nmcli device disconnect wlo1"
 
+abbr -a w "python2 -m SimpleHTTPServer 8000"
+
 # Aliases
 alias sl "ls"
 alias m "micro"
