7468c640ce99079e890475ab445e0da1ff9d38d4
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-20 19:14:51 -0400

    stumpwm setup

--- aux-scripts/setup-stump.fish
+++ aux-scripts/setup-stump.fish
@@ -1,8 +1,11 @@
 #!/usr/bin/env fish
 
-ros build ./stumpwm.ros
-install -m 0755 ./stumpwm /usr/local/bin
-rm -f ./stumpwm
+# Setup dependencies
+sbcl --eval "(ql:quickload '(clx-truetype swank stumpwm cl-strings inferior-shell))" --quit
+
+echo -e "#!/usr/bin/env bash
+sbcl --load /home/lepisma/cfg/misc/run-stumpwm.lisp" | sudo tee /usr/local/bin/stumpwm-sbcl > /dev/null
+
+mkdir -p ~/stumpwm/local
 git clone https://github.com/stumpwm/stumpwm-contrib ~/stumpwm/contrib
-mkdir ~/stumpwm/local
 git clone git@github.com:lepisma/swm-calibre ~/stumpwm/local/swm-calibre
---
+++ misc/run-stumpwm.lisp
@@ -0,0 +1,2 @@
+(require :stumpwm)
+(stumpwm:stumpwm)
