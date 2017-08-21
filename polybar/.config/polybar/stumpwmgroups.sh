d6ac92543bf4ee037cb7bc5d0c77052989ebf115
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-21 16:09:31 -0400

    greenish accent

--- polybar/.config/polybar/stumpwmgroups.sh
+++ polybar/.config/polybar/stumpwmgroups.sh
@@ -9,6 +9,6 @@ echo "
                                                            ((string-equal name \"FLOAT\") \"   FLOAT  \")
                                                            (t (concat \"   \" name \"  \")))))
                                   (if (eq g (current-group))
-                                      (concat \"%{F#fff B#111 u#eee +u}\" display-text \"%{F- B- u- -u}\")
+                                      (concat \"%{F#fff B#111 u#16A085 +u}\" display-text \"%{F- B- u- -u}\")
                                       (concat \"%{F#777}\" display-text \"%{F-}\"))))
                               (sort (screen-groups (current-screen)) #'< :key #'group-number)))" | stumpish -e eval
