188dd95be3df15ea6125b88fe6e79483d23a8c5e
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-22 19:40:16 -0400

    Automate monitor restriction

--- fish/.config/fish/functions/huion-setup-keys.fish
+++ fish/.config/fish/functions/huion-setup-keys.fish
@@ -1,8 +1,3 @@
-# NOTE: For restricting to a monitor
-# https://askubuntu.com/questions/839161/limit-a-graphics-tablet-to-one-monitor
-#
-# xinput map-to-output <id-of-stylus-via-xinput> <monitor-id-via-xrandr>
-
 function huion-setup-keys -d "Huion tablet setup"
   xsetwacom --set "HUION PenTablet Pad pad" Button 1 "key +ctrl +z -z -ctrl"
   xsetwacom --set "HUION PenTablet Pad pad" Button 2 "key e"
@@ -12,4 +7,9 @@ function huion-setup-keys -d "Huion tablet setup"
   xsetwacom --set "HUION PenTablet Pad pad" Button 10 "key ]"
   xsetwacom --set "HUION PenTablet Pad pad" Button 11 "key ["
   xsetwacom --set "HUION PenTablet Pad pad" Button 12 "key p"
+
+  # https://askubuntu.com/questions/839161/limit-a-graphics-tablet-to-one-monitor
+  # Get monitor via xrandr
+  set -l monitor "HDMI1"
+  xinput map-to-output (xinput | grep "Pen stylus" | cut -f 2 | cut -c 4-5) $monitor
 end
