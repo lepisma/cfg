1421813f7e04779ea2a37a0997e23900204b018d
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-09-09 02:15:14 -0400

    remove mpd functions

--- fish/.config/fish/music.fish
+++ fish/.config/fish/music.fish
@@ -1,70 +1,9 @@
 ## Music related functions
 
-# MPD Control
-function mp
-  if count $argv > /dev/null
-    switch $argv[1]
-      case "all"
-        echo "Playing all songs on shuffle"
-        mpc clear; and mpc ls | mpc add; and mpc shuffle; and mpc play
-      case "up"
-        mpc update --wait
-      case "l"
-        # Love track on last.fm
-        mpc sendmessage mpdas love
-      case "s"
-        echo "Running scrobbler"
-        mpdas &
-      case "*"
-        mpc clear; and mpc search $argv | mpc add; and mpc play
-    end
-  end
-end
-
-# All music player controls
-# --------------------------
-
-function _mpd_playing
-  switch (string join " " (mpc))
-    case "*[playing]*"
-      return 0
-    case "*"
-      return 1
-  end
-end
-
-function _work_on_mpd
-  # Check if mpd is playing
-  if mpc > /dev/null 2>&1; and _mpd_playing
-    return 0
-  else if sp > /dev/null 2>&1
-    # spotify is running
-    return 1
-  else
-    # Spotify not running
-    if mpc > /dev/null 2>&1
-      # Mpd running, toggle
-      return 0
-    else
-      return 1
-    end
-  end
-end
-
-function _work_on_spotify
-  if sp > /dev/null 2>&1
-    return 0
-  else
-    return 1
-  end
-end
-
 function player-toggle
-  if true
+  if bbq :current > /dev/null 2>&1
     bbq :toggle
-  else if _work_on_mpd
-    mpc toggle > /dev/null 2>&1
-  else if _work_on_spotify
+  else if sp > /dev/null 2>&1
     sp play
   else
     return 1
@@ -72,11 +11,9 @@ function player-toggle
 end
 
 function player-next
-  if true
+  if bbq :current > /dev/null 2>&1
     bbq :next
-  else if _work_on_mpd
-    mpc next > /dev/null 2>&1
-  else if _work_on_spotify
+  else if sp > /dev/null 2>&1
     sp next
   else
     return 1
@@ -84,11 +21,9 @@ function player-next
 end
 
 function player-prev
-  if true
+  if bbq :current > /dev/null 2>&1
     bbq :prev
-  else if _work_on_mpd
-    mpc prev > /dev/null 2>&1
-  else if _work_on_spotify
+  else if sp > /dev/null 2>&1
     sp prev
   else
     return 1
