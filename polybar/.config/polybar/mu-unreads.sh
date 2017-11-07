0051fc4067620341122e69375ea0357336f92b21
Abhinav Tushar <lepisma@fastmail.com>
2017-11-07 02:09:48 -0500

    Fix mu-unreads.sh

--- polybar/.config/polybar/mu-unreads.sh
+++ polybar/.config/polybar/mu-unreads.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 # Check for unread count in mu
-MU_OUT=$(emacsclient --eval '(s-trim (shell-command-to-string (concat "mu find " (mu4e-unread-bm-query) " | wc -l")))')
+MU_OUT=$(emacsclient --eval '(s-trim (shell-command-to-string (concat "mu find " (rogue-mu4e-unread-bm-query) " | wc -l")))')
 
-if (( ${#MU_OUT} > 0 )); then
-    echo $MU_OUT
+if [[ $MU_OUT == \"mu* ]]; then
+    echo "0"
 else
-    echo "NA"
+    echo $MU_OUT
 fi
