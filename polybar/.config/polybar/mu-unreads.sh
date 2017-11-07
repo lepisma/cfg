9f8d74557978c3bfc794391a544b6a912f295173
Abhinav Tushar <lepisma@fastmail.com>
2017-11-07 02:16:08 -0500

    Handle emacs error in mu-unreads.sh

--- polybar/.config/polybar/mu-unreads.sh
+++ polybar/.config/polybar/mu-unreads.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
 # Check for unread count in mu
-MU_OUT=$(emacsclient --eval '(s-trim (shell-command-to-string (concat "mu find " (rogue-mu4e-unread-bm-query) " | wc -l")))')
+MU_OUT=$(emacsclient --eval '(s-trim (shell-command-to-string (concat "mu find " (rogue-mu4e-unread-bm-query) " | wc -l")))' 2> /dev/null)
 
 if [[ $MU_OUT == \"mu* ]]; then
     echo "0"
+elif [[ $MU_OUT == "" ]]; then
+    echo "NA"
 else
     echo $MU_OUT
 fi
