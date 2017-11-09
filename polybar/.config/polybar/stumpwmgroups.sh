8189e6f2af307b6b1894d23d7a5cc5dac08c36ea
Abhinav Tushar <lepisma@fastmail.com>
2017-11-09 01:43:00 -0500

    Show number of windows in polybar

--- polybar/.config/polybar/stumpwmgroups.sh
+++ polybar/.config/polybar/stumpwmgroups.sh
@@ -4,11 +4,12 @@ echo "
 (apply #'concatenate 'string (mapcar
                               (lambda (g)
                                 (let* ((name (string-upcase (group-name g)))
-                                       (display-text (cond ((string-equal name \"MAIN\" ) \"   MAIN  \")
-                                                           ((string-equal name \"DOCS\") \"   DOCS  \")
-                                                           ((string-equal name \"FLOAT\") \"   FLOAT  \")
+                                       (n-win (write-to-string (length (group-windows g))))
+                                       (display-text (cond ((string-equal name \"MAIN\" ) \"   MAIN \")
+                                                           ((string-equal name \"DOCS\") \"   1 DOCS \")
+                                                           ((string-equal name \"FLOAT\") \"   2 FLOAT \")
                                                            (t (concat \"   \" name \"  \")))))
                                   (if (eq g (current-group))
-                                      (concat \"%{F#fff B#dd8a9899 u#8a9899 +u}\" display-text \"%{F- B- u- -u}\")
-                                      (concat \"%{F#333}\" display-text \"%{F-}\"))))
+                                      (concat \"%{F#fff B#dd8a9899 u#8a9899 +u}\" display-text \"[\" n-win \"] \" \"%{F- B- u- -u}\")
+                                      (concat \"%{F#333}\" display-text \"[\" n-win \"] \" \"%{F-}\"))))
                               (sort (screen-groups (current-screen)) #'< :key #'group-number)))" | stumpish -e eval
