27495d0cd0edac78adf1b3c675e3b91be5557dfe
Abhinav Tushar <lepisma@fastmail.com>
2017-11-29 02:59:14 -0500

    Colors

--- polybar/.config/polybar/stumpwmgroups.sh
+++ polybar/.config/polybar/stumpwmgroups.sh
@@ -10,6 +10,6 @@ echo "
                                                            ((string-equal name \"FLOAT\") \"   FLOAT \")
                                                            (t (concat \"   \" name \"  \")))))
                                   (if (eq g (current-group))
-                                      (concat \"%{F#fff B#dd8a9899 u#8a9899 +u}\" display-text \"[\" n-win \"] \" \"%{F- B- u- -u}\")
-                                      (concat \"%{F#333}\" display-text \"[\" n-win \"] \" \"%{F-}\"))))
+                                      (concat \"%{F#eeeeee B#3B4252 u#8a9899 +u}\" display-text \"[\" n-win \"] \" \"%{F- B- u- -u}\")
+                                      (concat \"%{F#8a9899}\" display-text \"[\" n-win \"] \" \"%{F-}\"))))
                               (sort (screen-groups (current-screen)) #'< :key #'group-number)))" | stumpish -e eval
