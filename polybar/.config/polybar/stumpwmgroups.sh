71821b81570f72bc780ec6d4f292a80ba0ce50b7
Abhinav Tushar <lepisma@fastmail.com>
2017-11-29 15:17:53 -0500

    Accent fix

--- polybar/.config/polybar/stumpwmgroups.sh
+++ polybar/.config/polybar/stumpwmgroups.sh
@@ -10,6 +10,6 @@ echo "
                                                            ((string-equal name \"FLOAT\") \"   FLOAT \")
                                                            (t (concat \"   \" name \"  \")))))
                                   (if (eq g (current-group))
-                                      (concat \"%{F#eeeeee B#3B4252 u#8a9899 +u}\" display-text \"[\" n-win \"] \" \"%{F- B- u- -u}\")
-                                      (concat \"%{F#8a9899}\" display-text \"[\" n-win \"] \" \"%{F-}\"))))
+                                      (concat \"%{F#ECEFF4 B#2E3440 u#8A9899 +u}\" display-text \"[\" n-win \"] \" \"%{F- B- u- -u}\")
+                                      (concat \"%{F#8A9899}\" display-text \"[\" n-win \"] \" \"%{F-}\"))))
                               (sort (screen-groups (current-screen)) #'< :key #'group-number)))" | stumpish -e eval
