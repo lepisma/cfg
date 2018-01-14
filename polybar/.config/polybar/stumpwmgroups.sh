8c3bc0cca80366978e2fdb15a3230ebe14219013
Abhinav Tushar <lepisma@fastmail.com>
2018-01-14 17:01:48 -0500

    Some transparency changes

--- polybar/.config/polybar/stumpwmgroups.sh
+++ polybar/.config/polybar/stumpwmgroups.sh
@@ -10,6 +10,6 @@ echo "
                                                            ((string-equal name \"FLOAT\") \"   FLOAT \")
                                                            (t (concat \"   \" name \"  \")))))
                                   (if (eq g (current-group))
-                                      (concat \"%{F#ECEFF4 B#2E3440 u#8A9899 +u}\" display-text \"[\" n-win \"] \" \"%{F- B- u- -u}\")
+                                      (concat \"%{F#ECEFF4 B#882E3440 u#8A9899 +u}\" display-text \"[\" n-win \"] \" \"%{F- B- u- -u}\")
                                       (concat \"%{F#8A9899}\" display-text \"[\" n-win \"] \" \"%{F-}\"))))
                               (sort (screen-groups (current-screen)) #'< :key #'group-number)))" | stumpish -e eval
