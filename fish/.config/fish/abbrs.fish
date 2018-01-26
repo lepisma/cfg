084a24a7353ac479fdbfb412497dddce25bb6200
Abhinav Tushar <lepisma@fastmail.com>
2018-01-25 23:35:43 -0500

    Move openconnect config to separate file

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -8,11 +8,10 @@ abbr -a yt "youtube-dl --external-downloader=aria2c"
 abbr -a ya "youtube-dl -x --audio-format opus --audio-quality 0 --no-playlist"
 
 abbr -a x "xdg-open"
-abbr -a v "sudo openconnect --juniper sslvpn.umass.edu"
+abbr -a v "sudo openconnect --config=/home/lepisma/.openconnect sslvpn.umass.edu"
 abbr -a d "nmcli device disconnect wlo1"
 abbr -a w "python2 -m SimpleHTTPServer 8000"
 
-# Aliases
 alias sl "ls"
 alias m "mumacs"
 alias dl "aria2c"
