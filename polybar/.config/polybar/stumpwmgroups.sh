7411c0af8b8d059370a895489d2d8d72b59ac3ab
Abhinav Tushar <lepisma@fastmail.com>
2018-04-18 20:49:25 -0400

    Move stumpwmgroups code in main .stumpwmrc file as suggested by @jcf

--- polybar/.config/polybar/stumpwmgroups.sh
+++ polybar/.config/polybar/stumpwmgroups.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
 
-echo "
-(apply #'concatenate 'string (mapcar
-                              (lambda (g)
-                                (let* ((name (string-upcase (group-name g)))
-                                       (n-win (write-to-string (length (group-windows g))))
-                                       (display-text (cond ((string-equal name \"MAIN\" ) \"   MAIN \")
-                                                           ((string-equal name \"CANVAS\") \"   CANVAS \")
-                                                           ((string-equal name \"FLOAT\") \"   FLOAT \")
-                                                           (t (concat \"   \" name \"  \")))))
-                                  (if (eq g (current-group))
-                                      (concat \"%{F#ECEFF4 B#882E3440 u#8A9899 +u}\" display-text \"[\" n-win \"] \" \"%{F- B- u- -u}\")
-                                      (concat \"%{F#8A9899}\" display-text \"[\" n-win \"] \" \"%{F-}\"))))
-                              (sort (screen-groups (current-screen)) #'< :key #'group-number)))" | stumpish -e eval
+stumpish eval "(stumpwm::polybar-groups)"
