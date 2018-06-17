ef59ece98546fbeb0fd50d8e32e20a664f11ab68
Abhinav Tushar <lepisma@fastmail.com>
2018-06-17 10:37:23 +0530

    Name based authinfo getters

--- offlineimap/offlineimapext.py
+++ offlineimap/offlineimapext.py
@@ -10,10 +10,10 @@ def plist_get(key, plist):
             return plist[1 + (i * 2)]
     return None
 
-def get_authinfo_value(machine, port, key):
+def get_authinfo_value(name, key):
     machines = [
         line.decode("utf-8").split(" ")
         for line in subprocess.check_output(["gpg", "--use-agent", "--quiet", "--batch", "-d", "/home/lepisma/.authinfo.gpg"]).strip().splitlines()
     ]
-    item = list(filter(lambda x: plist_get("machine", x) == machine and plist_get("port", x) == port, machines))[0]
+    item = list(filter(lambda x: plist_get("name", x) == name, machines))[0]
     return plist_get(key, item)
