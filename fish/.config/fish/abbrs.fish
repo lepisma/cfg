fea5043fa85e0dcefc38c6fb2dc1fe8a75795c76
Abhinav Tushar <lepisma@fastmail.com>
2018-12-09 06:23:13 +0530

    switch to yay

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -1,14 +1,13 @@
-abbr -a i "pacaur --color=auto -S"
-abbr -a r "pacaur --color=auto -Rsn"
-abbr -a up "pacaur --color=auto -Syu"
-abbr -a cl "pacaur --color=auto -Rsn (pacaur -Qdtq); pacaur --color=auto -Scc"
+abbr -a i "yay -S"
+abbr -a r "yay -Rsn"
+abbr -a up "yay -Syu --devel"
+abbr -a cl "yay -Rsn (yay -Qdtq); yay -Scc"
 abbr -a s "sudo"
 
 abbr -a yt "youtube-dl --external-downloader=aria2c"
 abbr -a ya "youtube-dl -x --audio-format opus --audio-quality 0 --no-playlist"
 
 abbr -a x "xdg-open"
-abbr -a v "sudo openconnect --config=/home/lepisma/.openconnect sslvpn.umass.edu"
 abbr -a d "nmcli device disconnect wlp2s0"
 abbr -a w "live-server"
 
