e04caffec3a896b5ab018107fc7b0c2b16f40dae
Abhinav Tushar <abhinav@lepisma.xyz>
2020-04-01 08:31:15 +0530

    vterm printf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,3 +17,16 @@ end
 
 eval (dircolors -c ~/cfg/misc/dircolors)
 direnv hook fish | source
+
+function vterm_printf;
+    if [ -n "$TMUX" ]
+        # tell tmux to pass the escape sequences through
+        # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
