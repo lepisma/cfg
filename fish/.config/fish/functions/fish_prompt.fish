90e8f756e8bb5ff62d140ab33548c5cf98f73046
Abhinav Tushar <abhinav@lepisma.xyz>
2021-07-25 12:28:19 +0530

    change fish stderr redirection syntax

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,9 +1,9 @@
 function _git_branch_name
-    echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+    echo (command git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _git_is_dirty
-    echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+    echo (command git status -s --ignore-submodules=dirty 2>/dev/null)
 end
 
 function fish_prompt
