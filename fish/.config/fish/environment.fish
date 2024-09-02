020185b3b1dacb5f9b17bf48a5d066cb5040cb04
Abhinav Tushar <abhinav@lepisma.xyz>
2024-09-02 17:51:51 +0530

    Clean up env file

--- fish/.config/fish/environment.fish
+++ fish/.config/fish/environment.fish
@@ -12,22 +12,8 @@ set -x BROWSER www
 set -a PATH ~/bin
 set -a PATH ~/.local/bin
 set -a PATH ~/.cask/bin
-set -a PATH ~/.roswell/bin
-set -a PATH (rbenv prefix)/bin
 set -a PATH ~/.cargo/bin
-set -a PATH ~/.pyenv/bin
-set -a PATH ~/.rbenv/bin
-set -a PATH ~/.poetry/bin
-set -a PATH ~/go/bin
-
 set -a PATH ~/.config/rofi/bin
 
-status is-interactive; and pyenv init --path | source
-pyenv init - | source
-status --is-interactive; and source (rbenv init -|psub)
-
 set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
-
-set -x QT_QPA_PLATFORMTHEME "qt5ct"
-
 set -a LD_LIBRARY_PATH /usr/local/lib
